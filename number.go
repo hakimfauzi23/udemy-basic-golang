30d44d172332f58f9ba8c5ef19bfdffa75852350
Hanif <hanif.hakim@spesolution.com>
2023-01-05 21:47:57 +0700

    learning number, boolean, and string data type in Go

---
+++ number.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("One = ", 1)                // int
+	fmt.Println("Two = ", 2)                // int
+	fmt.Println("Three point five = ", 3.5) //floating point
+}
---
+++ string.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hanif")
+	fmt.Println("Hanif Fauzi") // this is string data type
+	fmt.Println("Hanif Fauzi Hakim")
+
+	// some function for string data type
+	fmt.Println(len("Hanif"))
+	fmt.Println("Hanif Fauzi"[0])       // 72 is byte code for 'H'
+	fmt.Println("Hanif Fauzi Hakim"[1]) // 97 is byte code for "a"
+
+}
